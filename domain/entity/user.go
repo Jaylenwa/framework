33129f78d1484bf577d2bed26487c6ed06254a56
ProgramLion <1318248167@qq.com>
2023-06-27 16:14:37 +0800

    修改框架

--- domain/entity/user.go
+++ domain/entity/user.go
@@ -1,22 +1,9 @@
 package entity
 
-import (
-	"context"
-	"framework/infra/po"
-	"framework/infra/utils/struct"
-)
-
 type User struct {
 	Id   uint64 `json:"id"`
 	Name string `json:"name"`
 	Sex  int    `json:"sex"`
 	Age  int    `json:"age"`
 	Home string `json:"home"`
-}
-
-func (do User) ToPO(ctx context.Context) (po po.User, err error) {
-
-	err = _struct.CopyStruct(&po, do)
-
-	return
 }
